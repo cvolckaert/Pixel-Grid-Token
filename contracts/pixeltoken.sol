76aaecdbafe9d1b41924b19309cf1f7f73eae5b9
cvolckaert <cvolckaert@gmail.com>
2020-04-19 16:29:23 -0400

    Added basic token contract

--- contracts/pixeltoken.sol
+++ contracts/pixeltoken.sol
@@ -1,2 +1,22 @@
 pragma solidity ^0.6.4;
 
+import '../node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol';
+
+contract PixelToken is ERC721Token {
+    constructor(string _name, string_symbol)
+{}
+
+
+/*
+* Custom function to mint pixel hex grid to token URI
+*/
+
+function mintToken(
+   address _to,
+    uint256 _tokenId,
+    string _tokenURI
+)public{
+    super.mint(_to, _tokenId);
+    super.setTokenURI(_tokenId, _tokenURI);
+}
+}
