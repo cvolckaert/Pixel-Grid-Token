317bd09b39530c93a0264790163fbab5433e5350
cvolckaert <cvolckaert@gmail.com>
2020-04-22 18:49:32 -0400

    manually changed local app.jsx file to merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { PixelGrid, Pixel, Heading, Row, AppWrapper } from './styles/App';
-import Web3 from 'web3'
+import Web3 from 'web3';
 
 const { Component } = React;
-
-/// build/contracts/PixelToken.json
 
 const generateHex = () => {
   let length = 6;
@@ -29,9 +27,21 @@ export class App extends Component {
     super(props);
 
     this.state = {
-      grid: Array(16).fill(Array(16).fill(""))
+      grid: Array(16).fill(Array(16).fill("")),
+      account: ''
     }
   };
+
+  async loadBlockchainData() {
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
+    const accounts = await web3.eth.getAccounts()
+    this.setState({account: accounts[0]})
+  }
+  
+
+  componentWillMount() {
+    this.loadBlockchainData()
+  }
 
   componentDidMount() {
     console.log(generateGrid(this.state.grid))
